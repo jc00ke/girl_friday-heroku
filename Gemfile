03a11cae1fa8aaf5e6ce1e9c67d4f031150166e3
jc00ke <jesse@jc00ke.com>
2011-05-05 21:11:51 -0700

    Super basic queue working locally

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source :rubygems
 gem 'sinatra'
 gem 'girl_friday'
 gem 'redis'
+gem 'haml'
 
 group :development do
   gem 'heroku'
+  gem 'thin'
 end
--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
+  configure :production do
+    set :uri, URI.parse(ENV['REDISTOGO_URL'])
+  end
+  configure :development do
+    RedisURI = Struct.new :host, :port, :password
+    set :uri, RedisURI.new('localhost', 6379, nil)
+  end
+  configure do
+    set :store_config, {
+      :host       => uri.host,
+      :port       => uri.port,
+      :password   => uri.password
+    }
+  end
+  REDIS = Redis.new store_config
+
+  QUEUE = GirlFriday::WorkQueue.new('dat_q', :store => GirlFriday::Store::Redis, :store_config => [store_config]) do |msg|
+    REDIS.rpush('messages', msg)
+  end
+
   get '/' do
-    "hi!"
+    @messages = REDIS.lrange('messages', 0, -1)
+    haml :index
+  end
+
+  post '/' do
+    QUEUE << params[:message]
+    redirect to('/')
   end
 end
